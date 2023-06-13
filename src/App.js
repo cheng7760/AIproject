1992ba000c16242594df5a3522d8c0d5c892d4fa
Cyrus Reed <134946188+Cyrus-Reed@users.noreply.github.com>
2023-06-13 16:30:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ function App() {
         <h2>ChatGPT-Next-Web</h2>
         <p>这是一个ChatGPT网页用户界面。它的设计精美，用户体验优秀，支持Markdown格式，包括LaTex、mermaid、代码高亮等。它还支持响应式设计，深色模式和PWA。此外，它还有一个新的v2版本，可以创建、分享和调试你的聊天工具，使用提示模板（mask）。</p>
         <a href="https://pan.baidu.com/s/1mR6mT0IETcfs5fXT1jSe5Q?pwd=pw9b" className="button">查看教程</a>
+        <a href="https://chatgpt.cyrus-reed.com" className="button">进入站点</a>
       </div>
       <div className="project">
         <h2>go-proxy-bingai</h2>
         <p>这是一个使用Vue3和Go构建的微软New Bing演示站点，拥有一致的UI体验，支持ChatGPT提示词，国内可用。它基本兼容微软Bing AI的所有功能，无需登录即可畅聊。</p>
         <a href="https://pan.baidu.com/s/1lO7cp3YmuGJe2IwZar0wCQ?pwd=fb2o" className="button">查看教程</a>
+        <a href="https://bing.cyrus-reed.com" className="button">进入站点</a>
       </div>
     </div>
   );
