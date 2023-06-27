af33d7b4d3d3490376ca45251c873e4adb2f00e6
Cyrus Reed <134946188+Cyrus-Reed@users.noreply.github.com>
2023-06-27 23:24:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,20 +12,28 @@ function App() {
        </div>
       </div>
       <div className="project">
-        <h2><img src="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/icons/ChatGPT-Next-Web.png" alt="ChatGPT Icon" className="project-icon" /> ChatGPT</h2>
-        <p>ChatGPT是一个由OpenAI开发和发布的人工智能聊天机器人，基于GPT-3.5语言模型。现已接入业界最先进的AI绘图技术Midjourney。用户用/mj为前缀输入绘画提示词，例如：/mj a cat，即可生成绘画，速度较慢，生成中请勿重复提交或刷新页面，目前只支持英文提示词。</p>
+        <h2><img src="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/icons/ChatGPT.png" alt="ChatGPT Icon" className="project-icon" /> ChatGPT</h2>
+        <p>ChatGPT是一个人工智能聊天机器人，由OpenAI开发和发布。它基于GPT-3.5语言模型，可以以对话的形式与用户交互。ChatGPT可以根据用户的意图，提供个性化和专业的回答，分享自己的知识，引导用户探索更多的话题，遵守道德和安全的原则。</p>
         <div className="button-group">
-          <a href="https://chatgpt.cyrus-reed.com" className="button">进入站点</a>
-          <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/ChatGPT-Next-Web.txt" className="button">查看教程</a>
+          <a href="https://openai.cyrus-reed.com" className="button">进入站点</a>
+          <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/ChatGPT.txt" className="button">查看教程</a>
         </div>
         <p className="small-text">（首次使用建议观看）</p>
       </div>
       <div className="project">
-        <h2><img src="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/icons/go-proxy-bing.png" alt="Bing AI Icon" className="project-icon" /> Bing AI</h2>
-        <p>Bing AI是一个全新的人工智能搜索引擎，由微软开发和发布，基于GPT-4语言模型。现已接入OpenAI自研的AI绘图技术DALL·E2。用户需要选择更有创造力对话样式，输入绘画描述，例如：请帮我画一个小猪，即可生成绘画，速度较快，支持中文描述。</p>
+        <h2><img src="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/icons/Bing AI.png" alt="Bing AI Icon" className="project-icon" /> Bing AI</h2>
+        <p>Bing AI是一个全新的人工智能搜索引擎，由微软开发和发布。它基于OpenAI的GPT-4语言模型，这是目前最强大的语言模型，可以理解和生成各种类型的文本，包括视觉输入。其可以为用户提供更好的搜索，更完整的答案，更互动的聊天和更有创意的内容。</p>
         <div className="button-group">
           <a href="https://bing.cyrus-reed.com" className="button">进入站点</a>
-          <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/go-proxy-bing.txt" className="button">查看教程</a>
+          <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/Bing AI.txt" className="button">查看教程        </div>
+        <p className="small-text">（首次使用建议观看）</p>
+      </div>
+      <div className="project">
+        <h2><img src="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/icons/ChatGPT-Midjourney.png" alt="ChatGPT-Midjourney Icon" className="project-icon" /> ChatGPT-Midjourney</h2>
+        <p>Midjourney是一个由位于旧金山的独立研究实验开发和发布的生成性人工智能程序和服务。它能够根据自然语言描述，生成图像。Midjourney可以根据用户的需求，生成个性化和独特的艺术作品，分享其人工智能的创新成果，引导用户探索更多的艺术领域。</p>
+        <div className="button-group">
+          <a href="https://chatgpt.cyrus-reed.com" className="button">进入站点</a>
+          <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/ChatGPT-Midjourney.txt" className="button">查看教程</a>
         </div>
         <p className="small-text">（首次使用建议观看）</p>
       </div>
