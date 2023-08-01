c4cf730f38cbe21bc4a3a720b8259d8b9e81bbf9
Cyrus Reed <134946188+Cyrus-Reed@users.noreply.github.com>
2023-08-01 15:55:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
         <div className="category-line">
           <span>🏅 编辑推荐 🏅</span>
         </div>
-        <div className="project">
+        <div className="project" id="chatgpt-next-web">
           <h2><img src="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/icons/ChatGPT Next Web.png" alt="ChatGPT Next Web Icon" className="project-icon" /> ChatGPT Next Web</h2>
           <p className="project-description">本站点基于 OPENAI API 开发，内置了 KEY 让您无需登录就可以开始聊天，现已支持选择 GPT-4 模型，特点包括完整的 Markdown 支持，以及精心设计的 UI，响应式设计，支持深色模式，支持 PWA。此外，它还提供了预制角色功能，方便您进行个性化对话。无论您是在电脑端还是手机端，都可以享受到这个平台的便利，让您的聊天体验更加流畅。</p>
           <p className="project-description-mobile">本站点基于 OPENAI API 开发，内置了 KEY 让您无需登录就可以开始聊天，现已支持选择 GPT-4 模型，特点包括完整的 Markdown 支持，以及精心设计的 UI，响应式设计，支持深色模式，支持 PWA。</p>
