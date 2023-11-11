bc20f5d8e2926ec4ef08b081cabfa0335749fe8c
Cyrus Reed <134946188+Cyrus-Reed@users.noreply.github.com>
2023-11-11 14:49:50 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
           <p className="project-description-mobile">本站点基于 OPENAI API 开发，内置了 KEY 让您无需登录就可以开始聊天，现已支持联网搜索，可使用最先进的 GPT-4 模型，特点包括精心设计的 UI，响应式设计，支持深色模式，支持 PWA。</p>
           <div className="button-group">
             <a href="https://chatgpt-next-web.cyrus-reed.com" className="button">进入站点</a>
+            <a href="https://cloudflare-reverse-proxy.cyrus-reed.com/proxy/https://raw.githubusercontent.com/Cyrus-Reed/Cyrus-app/main/Android/ChatGPT_1.0.apk" className="button">下载应用</a>
             <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/ChatGPT Next Web.txt" className="button">查看教程</a>
           </div>
           <p className="small-text2">（首次使用建议观看）</p>
@@ -56,6 +57,7 @@ function App() {
           <p>本站点基于 OPENAI API 开发，内置了 KEY 让您无需登录就可以开始聊天，现已支持联网搜索，可使用最先进的 GPT-4 模型，特点包括精心设计的 UI，响应式设计，支持深色模式，支持 PWA。此外，它还提供了预制角色功能，方便您进行个性化对话。无论您是在电脑端还是手机端，都可以享受到这个平台的便利，让您的聊天体验更加流畅。</p>
           <div className="button-group">
             <a href="https://chatgpt-next-web.cyrus-reed.com" className="button">进入站点</a>
+            <a href="https://cloudflare-reverse-proxy.cyrus-reed.com/proxy/https://raw.githubusercontent.com/Cyrus-Reed/Cyrus-app/main/Android/ChatGPT_1.0.apk" className="button">下载应用</a>
             <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/ChatGPT Next Web.txt" className="button">查看教程</a>
           </div>
           <p className="small-text">（首次使用建议观看）</p>
