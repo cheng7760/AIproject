8deab16aec243f3099fdd2898124e401eaab9fb1
Cyrus Reed <134946188+Cyrus-Reed@users.noreply.github.com>
2023-11-28 15:41:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           <p className="project-description">本站点基于 OPENAI API 开发，内置了 KEY 让您无需登录就可以开始聊天，现已支持联网搜索，可使用最先进的 GPT-4 模型，特点包括精心设计的 UI，响应式设计，支持深色模式，支持 PWA。此外，它还提供了预制角色功能，方便您进行个性化对话。无论您是在电脑端还是手机端，都可以享受到这个平台的便利，让您的聊天体验更加流畅。</p>
           <p className="project-description-mobile">本站点基于 OPENAI API 开发，内置了 KEY 让您无需登录就可以开始聊天，现已支持联网搜索，可使用最先进的 GPT-4 模型，特点包括精心设计的 UI，响应式设计，支持深色模式，支持 PWA。</p>
           <div className="button-group">
-            <a href="https://chatgpt-next-web2.cyrus-reed.com" className="button">进入站点</a>
+            <a href="https://chatgpt-next-web3.cyrus-reed.com" className="button">进入站点</a>
             <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/ChatGPT Next Web.txt" className="button">查看教程</a>
           </div>
           <p className="small-text2">（首次使用建议观看）</p>
@@ -53,7 +53,7 @@ function App() {
           <h2><img src="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/icons/ChatGPT Next Web.png" alt="ChatGPT Next Web Icon" className="project-icon" /> ChatGPT Next Web</h2>
           <p>本站点基于 OPENAI API 开发，内置了 KEY 让您无需登录就可以开始聊天，现已支持联网搜索，可使用最先进的 GPT-4 模型，特点包括精心设计的 UI，响应式设计，支持深色模式，支持 PWA。此外，它还提供了预制角色功能，方便您进行个性化对话。无论您是在电脑端还是手机端，都可以享受到这个平台的便利，让您的聊天体验更加流畅。</p>
           <div className="button-group">
-            <a href="https://chatgpt-next-web2.cyrus-reed.com" className="button">进入站点</a>
+            <a href="https://chatgpt-next-web3.cyrus-reed.com" className="button">进入站点</a>
             <a href="https://raw.gitmirror.com/Cyrus-Reed/Cyrus-website/main/public/faqs/ChatGPT Next Web.txt" className="button">查看教程</a>
           </div>
           <p className="small-text">（首次使用建议观看）</p>
