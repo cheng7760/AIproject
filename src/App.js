550bdc8f6c52e725fd68025fea657c965e664af8
Cyrus-Reed-US <152876591+Cyrus-Reed-US@users.noreply.github.com>
2023-12-06 22:49:14 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,15 +21,15 @@ function App() {
         </div>
         <div className="project" id="chatgpt-next-web">
           <h2><img src="https://raw.gitmirror.com/Cyrus-Reed-US/Cyrus-website/main/public/icons/ChatGPT by Cyrus.png" alt="ChatGPT Next Web Icon" className="project-icon" /> ChatGPT by Cyrus</h2>
-          <p className="project-description">本站点基于 ChatGPT Web 开发，采用加密数据库实现了用户登录、注册和管理等功能，同时支持多端数据同步，对话记录与账号绑定，保护您的数据安全。普通用户可使用 GPT-3.5-Turbo 模型，高级用户可使用最先进的 GPT-4 模型。系统接入百度云内容审核平台，积极落实《生成式人工智能服务管理暂行办法》，有效提升内容安全性。</p>
-          <p className="project-description-mobile">本站点基于 ChatGPT Web 开发，采用加密数据库实现了用户登录、注册和管理等功能，同时支持多端数据同步，对话记录与账号绑定，保护您的数据安全。普通用户可使用 GPT-3.5-Turbo 模型，高级用户可使用最先进的 GPT-4 模型。</p>
+          <p className="project-description">本站点基于 ChatGPT Web 开发，采用加密数据库实现了用户登录、注册和管理等功能，同时支持多端数据同步，对话记录与账号绑定，保护您的数据安全。普通访问可使用 GPT-3.5-Turbo 模型，资深访问可使用最先进的 GPT-4 模型。系统使用加密传输，后台无法获取您的任何消息记录，从而最大程度的保护了您的隐私安全。</p>
+          <p className="project-description-mobile">本站点基于 ChatGPT Web 开发，采用加密数据库实现了用户登录、注册和管理等功能，同时支持多端数据同步，对话记录与账号绑定，保护您的数据安全。普通访问可使用 GPT-3.5-Turbo 模型，资深访问可使用最先进的 GPT-4 模型。</p>
           <div className="button-group">
             <a href="https://chatgpt-web.cyrus-reed.com" className="button">进入站点</a>
             <a href="https://raw.gitmirror.com/Cyrus-Reed-US/Cyrus-website/main/public/faqs/ChatGPT by Cyrus.txt" className="button">查看教程</a>
           </div>
           <p className="small-text2">（首次使用建议观看）</p>
         </div>
-        <p className="recommendation">推荐理由：功能丰富，拥有用户登录、注册和管理等多端数据同步功能，最高支持 GPT-4 模型，内容安全。</p>
+        <p className="recommendation">推荐理由：功能丰富，拥有用户登录、注册和管理等多端数据同步功能，最高支持 GPT-4 模型，隐私安全。</p>
       </div>
         <div className="category">
           <div className="category-line">
@@ -51,7 +51,7 @@ function App() {
         </div>
         <div className="project">
           <h2><img src="https://raw.gitmirror.com/Cyrus-Reed-US/Cyrus-website/main/public/icons/ChatGPT by Cyrus.png" alt="ChatGPT Next Web Icon" className="project-icon" /> ChatGPT by Cyrus</h2>
-          <p>本站点基于 ChatGPT Web 开发，采用加密数据库实现了用户登录、注册和管理等功能，同时支持多端数据同步，对话记录与账号绑定，保护您的数据安全。普通用户可使用 GPT-3.5-Turbo 模型，高级用户可使用最先进的 GPT-4 模型。系统接入百度云内容审核平台，积极落实《生成式人工智能服务管理暂行办法》，有效提升内容安全性。</p>
+          <p>本站点基于 ChatGPT Web 开发，采用加密数据库实现了用户登录、注册和管理等功能，同时支持多端数据同步，对话记录与账号绑定，保护您的数据安全。普通访问可使用 GPT-3.5-Turbo 模型，资深访问可使用最先进的 GPT-4 模型。系统使用加密传输，后台无法获取您的任何消息记录，从而最大程度的保护了您的隐私安全。</p>
           <div className="button-group">
             <a href="https://chatgpt-web.cyrus-reed.com" className="button">进入站点</a>
             <a href="https://raw.gitmirror.com/Cyrus-Reed-US/Cyrus-website/main/public/faqs/ChatGPT by Cyrus.txt" className="button">查看教程</a>
